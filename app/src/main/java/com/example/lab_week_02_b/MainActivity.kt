52483c86f86246dd02a50a23d5e21737c3eaef08
rattle90 <yuapedroza@gmail.com>
2025-09-10 13:36:18 +0700

    Commit No. 3 – Fix application crash when user input wrong color code [Intent]

--- app/src/main/java/com/example/lab_week_02_b/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_02_b/MainActivity.kt
@@ -9,14 +9,32 @@ import android.widget.Button
 import android.content.Intent
 import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.ActivityResult
 
 class MainActivity : AppCompatActivity() {
     companion object {
         private const val COLOR_KEY = "COLOR_KEY"
+        private const val ERROR_KEY = "ERROR_KEY"
     }
+
     private val submitButton: Button
         get() = findViewById(R.id.submit_button)
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                activityResult ->
+            val data = activityResult.data
+            val error = data?.getBooleanExtra(ERROR_KEY, false)
+            if(error == true){
+                Toast
+                    .makeText(this, getString(R.string.color_code_input_invalid),
+                        Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         submitButton.setOnClickListener{
@@ -25,14 +43,14 @@ class MainActivity : AppCompatActivity() {
             if(colorCode.isNotEmpty()){
                 if (colorCode.length < 6){
                     Toast
-                        .makeText(this,
-                            getString(R.string.color_code_input_wrong_length), Toast.LENGTH_LONG)
+                        .makeText(this, getString(R.string.color_code_input_wrong_length),
+                            Toast.LENGTH_LONG)
                         .show()
                 }
                 else{
                     val ResultIntent = Intent(this, ResultActivity::class.java)
                     ResultIntent.putExtra(COLOR_KEY, colorCode)
-                    startActivity(ResultIntent)
+                    startForResult.launch(ResultIntent)
                 }
             }
             else{
--- app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
+++ app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
@@ -1,30 +1,43 @@
-package com.example.lab_week_02_b
+    package com.example.lab_week_02_b
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import android.graphics.Color
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
+    import android.os.Bundle
+    import androidx.activity.enableEdgeToEdge
+    import androidx.appcompat.app.AppCompatActivity
+    import androidx.core.view.ViewCompat
+    import androidx.core.view.WindowInsetsCompat
+    import android.graphics.Color
+    import android.widget.TextView
+    import androidx.constraintlayout.widget.ConstraintLayout
+    import android.app.Activity
+    import android.content.Intent
 
-class ResultActivity : AppCompatActivity() {
-    companion object {
-        private const val COLOR_KEY = "COLOR_KEY"
-    }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
-        if(intent != null){
-            val colorCode = intent.getStringExtra(COLOR_KEY)
-            val backgroundScreen =
-                findViewById<ConstraintLayout>(R.id.background_screen)
-            backgroundScreen.setBackgroundColor(Color.parseColor("#$colorCode"))
-            val resultMessage =
-                findViewById<TextView>(R.id.color_code_result_message)
-            resultMessage.text = getString(R.string.color_code_result_message,
-                colorCode?.uppercase())
+    class ResultActivity : AppCompatActivity() {
+        companion object {
+            private const val COLOR_KEY = "COLOR_KEY"
+            private const val ERROR_KEY = "ERROR_KEY"
+        }
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_result)
+            if(intent != null){
+                val colorCode = intent.getStringExtra(COLOR_KEY)
+                val backgroundScreen =
+                    findViewById<ConstraintLayout>(R.id.background_screen)
+                try {
+                    backgroundScreen.setBackgroundColor(Color.parseColor("#$colorCode"))
+                }
+                catch (ex: IllegalArgumentException){
+                    Intent().let{
+                            errorIntent ->
+                        errorIntent.putExtra(ERROR_KEY, true)
+                        setResult(Activity.RESULT_OK, errorIntent)
+                        finish()
+                    }
+                }
+                val resultMessage =
+                    findViewById<TextView>(R.id.color_code_result_message)
+                resultMessage.text = getString(R.string.color_code_result_message,
+                    colorCode?.uppercase())
+            }
         }
     }
-}
